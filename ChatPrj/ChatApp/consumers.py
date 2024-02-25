6e24f8ef6fae3fb5847f84703a135ac93dbddac9
Alejandro Caceres <ale@Alejandros-MacBook-Pro.local>
2024-02-25 17:26:25 -0300

    Defer Django Imports to Fix App Registry Error

--- ChatPrj/ChatApp/consumers.py
+++ ChatPrj/ChatApp/consumers.py
@@ -1,9 +1,9 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from ChatApp.models import Message, Room
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    from ChatApp.models import Message, Room
     async def connect(self):
         self.room_name = f"room_{self.scope['url_route']['kwargs']['room_name']}"
         await self.channel_layer.group_add(self.room_name, self.channel_name)
