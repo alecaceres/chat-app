fc7083de5e6bde3a96632e95ceb524f6fce73e36
Alejandro Caceres <ale@Alejandros-MacBook-Pro.local>
2024-02-25 17:35:32 -0300

    Reorder get_asgi_application

--- ChatPrj/ChatPrj/asgi.py
+++ ChatPrj/ChatPrj/asgi.py
@@ -10,14 +10,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
+django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from ChatApp import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatPrj.settings')
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
